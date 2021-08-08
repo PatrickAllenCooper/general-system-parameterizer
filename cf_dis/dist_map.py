3deac0f0761f320ca8f40402c66498dd0ca7901b
PatrickAllenCooper <patrick.allen.cooper@gmail.com>
2021-08-08 16:53:54 -0600

    Description of first distribution generalizer added

---
+++ cf_dis/dist_map.py
@@ -0,0 +1,25 @@
+'''
+Patrick Cooper
+CAI
+8 Aug 2021
+
+This experiment seeks to evaluate the capacity of a network to understand
+generalizations gained through experimentation. Specifically we seek to
+determine how a network fairs at generally interpretting massively
+overparameterized distributions with only a select collection of examples.
+
+While the ultimate application of this technology is to engage simulations
+and real work counterfactual examinations more effectively, here it will
+suffice to withhold information from the network as though such data were
+undiscovered.
+
+Note: This data can be run within a notebook or else it may also be deployed
+
+'''
+
+# Experiment 1:
+# Synthetically generated data
+
+
+# Experiment 2:
+# Naturally occuring data
